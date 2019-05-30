bb069055499553970879addbc05ac9f90e66877b
Desarrollo <desarrollo@iMac-de-John.local>
2019-05-30 16:35:12 -0500

    fix

--- resources/js/web/Index.vue
+++ resources/js/web/Index.vue
@@ -6,7 +6,7 @@
                             <div class="title">
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 50 50"><path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z "/></svg>
                                 <h2>Busco</h2>
-                                <div class="check-group buscador">
+                                <div class="check-group buscador showto">
                                     <input type="checkbox" id="busco-1" name="inmueble" @click="buscarTipo('Casa')" class="unique" show="busco"><label for="busco-1" class="radio-btn">Casa</label>
                                     <input type="checkbox" id="busco-2" name="inmueble" class="unique"><label for="busco-2" class="radio-btn">Apto</label>
                                     <input type="checkbox" id="busco-3" name="inmueble" class="unique"><label for="busco-3" class="radio-btn">Oficina</label>
