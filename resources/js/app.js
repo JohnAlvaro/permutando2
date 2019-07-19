21228746c720c915db7453cf4a50f9fe66baed9f
John Alvaro <jhon54plex@hotmail.com>
2019-07-19 12:01:00 -0500

    Prueba de mapas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,7 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import { Icon } from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 
 require('./bootstrap');
 
@@ -30,6 +28,19 @@ Vue.component('web-index', require('./web/Index.vue'));
 Vue.component('tengo', require('./web/Tengo.vue'));
 Vue.component('busco', require('./web/Busco.vue'));
 Vue.component('leaflet', require('./web/Leaflet.vue'));
+
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 
 // let router = new Router({
--- resources/js/web/Leaflet.vue
+++ resources/js/web/Leaflet.vue
@@ -2,19 +2,31 @@
 <div>
     <div class="list">
             <div class="item"> 
-                <div  id="map">
-                        <h1>map</h1>
-                </div>
+                <l-map :zoom="zoom" :center="center">
+                    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+                    <l-marker :lat-lng="marker"></l-marker>
+                </l-map>   
             </div>
     </div>
 </div>
 </template>
 
 <script>
-
+import {LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 export default {
+
+    components: {
+        LMap,
+        LTileLayer,
+        LMarker
+    },
     data(){
         return{
+            zoom:13,
+      center: L.latLng(47.413220, -1.219482),
+      url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: L.latLng(47.413220, -1.219482),
 
         }
     },
@@ -30,3 +42,11 @@ export default {
     
 }
 </script>
+
+<style>
+ .item{
+  height: 30pc;
+  margin: 0;
+}
+</style>
+
